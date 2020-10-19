4a50e42f85f110fb37dc140786620a7bfe2a0b02
Maksym Garus <bboykermo@gmail.com>
2020-10-20 01:00:39 +0200

    test: add simple form tests

---
+++ shelter/tests/test_forms.py
@@ -0,0 +1,23 @@
+from django.test import SimpleTestCase
+from shelter.forms import SearchAnimalForm
+
+
+class TestForms(SimpleTestCase):
+
+    def test_search_animal_form_is_valid(self):
+        form = SearchAnimalForm(data={
+            'species': 'cat',
+            'name': 'skaza',
+            'size': 'big',
+            'age': 3,
+            'gender': 'male',
+            'chip_number': 2304982903,
+            'identification_number': 230420,
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_search_animal_form_no_data(self):
+        form = SearchAnimalForm(data={})
+
+        self.assertTrue(form.is_valid())
