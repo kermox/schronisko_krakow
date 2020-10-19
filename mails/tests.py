4ec553892f85afb36320ba4020d918023663bdf7
Maksym Garus <bboykermo@gmail.com>
2020-10-19 23:36:51 +0200

    test: add test for send_email function

--- mails/tests.py
+++ mails/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from mails.tasks import send_email
+from mails.models import EmailAddress, EmailTemplate
 
-# Create your tests here.
+
+class TestEmailSendFunction(TestCase):
+
+    def setUp(self):
+        EmailTemplate.objects.create(
+            title='test title',
+            body='test body'
+        )
+
+    def test_email_send(self):
+
+        address = 'bboykermo@gmail.com'
+
+        self.assertEquals(send_email(address), 'Message was sent')
