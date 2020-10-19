58507384189266620a870c1c7011e0b765fc5843
Maksym Garus <bboykermo@gmail.com>
2020-10-20 00:08:33 +0200

    test: add models tests

---
+++ news/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from news.models import Post, FacebookPost, Topic
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.post = Post.objects.create(
+            title='test title',
+            content='test content',
+        )
+        self.topic = Topic.objects.create(
+            name='test topic'
+        )
+
+    def test_post_is_assigned_slug_on_creation(self):
+        self.assertEquals(self.post.slug, 'test-title')
+
+    def test_topic_is_assigned_slug_on_creation(self):
+        self.assertEquals(self.topic.slug, 'test-topic')
