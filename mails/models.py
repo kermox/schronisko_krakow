d49e7c44510a80c1cbce769dcef31c0273525598
Maksym Garus <bboykermo@gmail.com>
2020-10-21 15:15:03 +0200

    feat(models): add inheritance from TimeStampMixin

--- mails/models.py
+++ mails/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import EmailValidator
+
+from utils.utils import TimeStampMixin
 
 
 class EmailTemplate(models.Model):
@@ -23,10 +24,16 @@ class EmailTemplate(models.Model):
         return self.email_body[0:20]
 
 
-class EmailBase(models.Model):
-    email_address = models.EmailField(blank=False, null=True,
-                                      validators=[
-                                          EmailValidator(message='Przepraszamy, podany adress jest niepoprawny.')])
+class EmailAddress(TimeStampMixin, models.Model):
+    address = models.EmailField(
+        blank=False,
+        null=True,
+        validators=[EmailValidator(message='Proszę podać prawidłowy adress.')]
+    )
+
+    class Meta:
+        verbose_name = 'Address'
+        verbose_name_plural = 'Addresses'
 
     def __str__(self):
         return self.email_address
