ad6eae15a35236ec0f744c2295915012ca19b38a
Maksym Garus <bboykermo@gmail.com>
2020-10-21 14:26:17 +0200

    feat: use RadioSelect widget instead of Select
    RadioSelect is much more easier to style

--- shelter/forms.py
+++ shelter/forms.py
@@ -2,16 +2,26 @@ from django import forms
 from .models import Animal
 
 SPECIES_CHOICES = [
-    ('', '-----'),
     ('dog', 'Pies'),
     ('cat', 'Kot'),
 ]
 
 SIZE_CHOICES = [
-    ('', '-----'),
     ('small', 'Mały'),
     ('medium', 'Średni'),
     ('big', 'Duży'),
+]
+
+AGE_CHOICES = [
+    (0, '0-1'),
+    (1, '1-3'),
+    (2, '3-5'),
+    (3, '5+'),
+]
+
+GENDER_CHOICES = [
+    ('male', 'Samiec'),
+    ('female', 'Samica'),
 ]
 
 
@@ -20,13 +30,13 @@ class SearchAnimalForm(forms.ModelForm):
         model = Animal
         fields = ('species', 'size', 'identification_number', 'chip_number', 'name')
         widgets = {
-            'species': forms.Select(),
-            'size': forms.Select(),
+            'species': forms.RadioSelect(),
+            'size': forms.CharField(),
             'identification_number': forms.TextInput(),
             'chip_number': forms.TextInput(),
             'name': forms.TextInput(),
+            'age': forms.RadioSelect(),
         }
-        # Pamiętać rzeby po stronie back endu dodać walidację do identification number i chip number
 
     name = forms.CharField(
         required=False,
@@ -34,9 +44,16 @@ class SearchAnimalForm(forms.ModelForm):
     species = forms.ChoiceField(
         choices=SPECIES_CHOICES,
         required=False,
+        widget=forms.RadioSelect()
     )
     size = forms.ChoiceField(
         choices=SIZE_CHOICES,
         required=False,
+        widget=forms.RadioSelect()
+    )
+    age = forms.ChoiceField(
+        choices=AGE_CHOICES,
+        required=False,
+        widget=forms.RadioSelect(),
     )
 
