a9ae7b2a9784fef2a7990a000a0ecf6be9f26c1a
Maksym Garus <bboykermo@gmail.com>
2020-10-21 16:30:56 +0200

    test: add some simple functional tests using selenium

---
+++ functional_tests/test_news_app.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from news.models import Post, Topic
+from django.contrib.staticfiles.testing import LiveServerTestCase
+from django.urls import reverse
+
+
+class TestNewsAppPages(LiveServerTestCase):
+
+    def setUp(self):
+        topic = Topic.objects.create(
+            name='topic name'
+        )
+
+        Post.objects.create(
+            title='no topic post',
+            content='some test content',
+            status='published'
+        )
+        Post.objects.create(
+            title='topic post',
+            content='some test content',
+            status='published',
+            topic=topic
+        )
+
+        self.browser = webdriver.Chrome('functional_tests/chromedriver')
+
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_news_posts_are_displayed(self):
+        self.browser.get(self.live_server_url + reverse('news-list'))
+
+        post = self.browser.find_element_by_class_name('card')
+
+        self.assertTrue(post)
+
+    def test_news_topics_are_displayed(self):
+        self.browser.get(self.live_server_url + reverse('news-list'))
+
+        topic_link = self.browser.find_element_by_link_text('topic name'.upper())
+
+        self.assertTrue(topic_link)
+
+    def test_news_topics_links_redirect_to_topic_detail(self):
+        self.browser.get(self.live_server_url + reverse('news-list'))
+
+        topic_url = self.live_server_url + reverse('news-topic-detail', args=['topic-name'])
+
+        topic_link = self.browser.find_element_by_link_text('topic name'.upper())
+        topic_link_clicked = topic_link.click()
+
+        self.assertEquals(self.browser.current_url, topic_url)
+
+    def test_news_topics_posts_are_displayed(self):
+        self.browser.get(self.live_server_url + reverse('news-topic-detail', args=['topic-name']))
+
+        post = self.browser.find_element_by_class_name('card')
+
+        self.assertTrue(post)
---
+++ functional_tests/test_shelter_app.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from shelter.models import Animal
+from django.contrib.staticfiles.testing import LiveServerTestCase
+from django.urls import reverse
+import time
+
+
+class TestShelterAppPages(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('functional_tests/chromedriver')
+
+        Animal.objects.create(
+            name='Rower',
+            age='2',
+            gender='male',
+            size='small',
+            species='cat',
+            photo='animal_photos/TEOS.jpeg'
+        )
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_animals_cards_display(self):
+        self.browser.get(self.live_server_url + reverse('animal-list'))
+
+        card = self.browser.find_element_by_class_name('card')
+
+        self.assertTrue(card)
+
+    def test_animal_list_link_redirects_to_animal_detail(self):
+        self.browser.get(self.live_server_url + reverse('animal-list'))
+
+        animal_detail = self.live_server_url + reverse('animal-detail', args=['rower'])
+        # causes ConnectionResetError, reason unknown but test is working right
+        self.browser.find_element_by_class_name('animal-card-link').click()
+
+        self.assertEquals(self.browser.current_url, animal_detail)
+
