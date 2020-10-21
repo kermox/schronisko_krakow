4df4e43d1f250603b487dd73039d233317c7562b
Maksym Garus <bboykermo@gmail.com>
2020-10-21 16:13:38 +0200

    not all changes were commited

--- mails/tasks.py
+++ mails/tasks.py
@@ -1,6 +1,4 @@
 from celery import shared_task
-
-from schronisko_krakow.settings import EMAIL_HOST_USER
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
@@ -9,10 +7,16 @@ from .models import EmailTemplates
 
 @shared_task()
 def send_email(address):
-    merge_data = {'email': EmailTemplates.objects.get().email_body}
+    """
+    Takes an html template and a record from EmailTemplates model and renders to string.
+    Creates an instance of EmailMultiAlternatives, populates it with necessary data and calls the 'send' method.
+    Then the html template is send to 'address' which is a string argument.
+    Returns the string confirmation that email was sent.
+    """
+    merge_data = {'email': EmailTemplate.objects.first().body}
     subject, from_email, to = 'Testowa wiadomość', EMAIL_HOST_USER, address
     text_content = 'This is an important message.'
-    html_content = render_to_string("mails/adoption-information-email.html", merge_data)
+    html_content = render_to_string("mails/subscribe-template.html", merge_data)
     message = EmailMultiAlternatives(subject, text_content, from_email, [to])
     message.attach_alternative(html_content, "text/html")
     message.send()
