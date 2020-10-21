7ebaee5c177f6b47169615ceddd085b9da607877
Maksym Garus <bboykermo@gmail.com>
2020-10-21 13:55:49 +0200

    feat(admin): update admin interface for mails app

--- mails/admin.py
+++ mails/admin.py
@@ -1,16 +1,33 @@
 from django.contrib import admin
-from mails.models import EmailTemplates, EmailBase
+
+from mails.models import EmailAddress, EmailTemplate
 
 
-class EmailTemplatesAdmin(admin.ModelAdmin):
-    pass
+class EmailTemplateAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        has_permission = super(EmailTemplateAdmin, self).has_add_permission(request)
+
+        # Check for the user permission and object instance.
+        if has_permission and EmailTemplate.objects.exists():
+            # If user has permission and there is already an instance in database,
+            # set the permission to False
+            has_permission = False
+        return has_permission
 
 
-class EmailBaseAdmin(admin.ModelAdmin):
-    pass
+class EmailAddressAdmin(admin.ModelAdmin):
+    search_fields = ['address', ]
+    readonly_fields = ['address', ]
+    date_hierarchy = 'created_at'
 
 
-admin.site.register(EmailTemplates, EmailTemplatesAdmin)
-admin.site.register(EmailBase, EmailBaseAdmin)
+    def has_change_permission(self, request, obj=None):
+        has_permission = super(EmailAddressAdmin, self).has_change_permission(request)
+        if has_permission:
+            has_permission = False
+        return has_permission
 
 
+admin.site.register(EmailTemplate, EmailTemplateAdmin)
+admin.site.register(EmailAddress, EmailAddressAdmin)
