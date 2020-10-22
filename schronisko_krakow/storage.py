bf5c27c4eda862888eb9609232025c3dddfa1046
Maksym Garus <bboykermo@gmail.com>
2020-10-22 19:51:22 +0200

    deployment: when DEBUG use StaticFilesStorage otherwise CompressedManifestStaticFilesStorage

--- schronisko_krakow/storage.py
+++ schronisko_krakow/storage.py
@@ -1,6 +1,10 @@
 from whitenoise.storage import CompressedManifestStaticFilesStorage, StaticFilesStorage
+from .settings import DEBUG
 
-
-# For tests should inherit StaticFilesStorage, otherwise CompressedManifestStaticFilesStorage
-class WhiteNoiseStaticFilesStorage(StaticFilesStorage):
-    manifest_strict = False
+# For tests should inherit StaticFilesStorage otherwise CompressedManifestStaticFilesStorage
+if DEBUG:
+    class WhiteNoiseStaticFilesStorage(StaticFilesStorage):
+        manifest_strict = False
+else:
+    class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
+        manifest_strict = False
