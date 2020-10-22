1d64b5e9be3873fd8e9425ee03f07d27ba7c0a35
Maksym Garus <bboykermo@gmail.com>
2020-10-22 17:14:50 +0200

    deployment: start using AWS S3 storage for mediafiles

--- schronisko_krakow/settings.py
+++ schronisko_krakow/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'shelter.apps.ShelterConfig',
     'widget_tweaks',
     'mails',
+    'storages',
     'whitenoise',
 ]
 
@@ -161,3 +162,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_src')
 MEDIA_URL = '/mediafiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
