da7d0c42e77e45c6220eaa30ae16b1e4d111ba56
Maksym Garus <bboykermo@gmail.com>
2020-10-23 20:25:32 +0200

    refactor: declare STATIC_ROOT path
    Although this PATH is already declared by django_on_heroku package, it is more clear to keep it visible.

--- schronisko_krakow/settings.py
+++ schronisko_krakow/settings.py
@@ -144,6 +144,8 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR + '/staticfiles/')
+
 STATICFILES_STORAGE = 'schronisko_krakow.storage.WhiteNoiseStaticFilesStorage'
 
 MEDIA_URL = '/mediafiles/'
