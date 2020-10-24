3b79322c9a6f5e08dc3d4de75c3ecaa8a6a7f6e1
Maksym Garus <bboykermo@gmail.com>
2020-10-24 21:49:21 +0200

    build: change admin url

--- schronisko_krakow/urls.py
+++ schronisko_krakow/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-schronisko/', admin.site.urls),
     path('', include('shelter.urls')),
     path('news/', include('news.urls')),
 ]
