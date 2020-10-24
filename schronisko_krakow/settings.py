19234116e8a534e5c9dd11775304927a07d0f020
Maksym Garus <bboykermo@gmail.com>
2020-10-24 22:28:22 +0200

    style: remove slash from url

--- schronisko_krakow/settings.py
+++ schronisko_krakow/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = (os.environ.get("DEBUG_VALUE") == 'True')
 
-ALLOWED_HOSTS = ['https://schroniskokrakow.herokuapp.com/', '192.168.0.10']
+ALLOWED_HOSTS = ['https://schroniskokrakow.herokuapp.com', '192.168.0.10']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
