c2174f76446402f41539c24519581df6f16126d2
Maksym Garus <bboykermo@gmail.com>
2020-10-25 11:48:15 +0100

    refactor: allow django_on_heroku only when DEBUG is False

--- schronisko_krakow/settings.py
+++ schronisko_krakow/settings.py
@@ -142,5 +142,5 @@ AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # HEROKU
-
-django_on_heroku.settings(locals())
+if not DEBUG:
+    django_on_heroku.settings(locals())
